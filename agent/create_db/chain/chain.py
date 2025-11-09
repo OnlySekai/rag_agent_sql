245f75068ad7b356d3a45f6d75430aeaa80b1b6f
onlysekai <minhtuan25902@gmail.com>
2025-11-09 17:47:36 +0700

    :+1: Norm

--- agent/create_db/chain/chain.py
+++ agent/create_db/chain/chain.py
@@ -1,10 +1,11 @@
 import os
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableLambda
 from agent.create_db.promt.parse_csv import parse_csv_promt
 from agent.create_db.entities.table_info import TableOutput
 from db.csv import parse_and_save_csv, save_metadata
 from llm.openai_api import llm_with_temp
-llm = llm_with_temp(1.2)
+
+llm = llm_with_temp(1.2).with_structured_output(TableOutput)
 
 def wrapper_save_csv(input: TableOutput):
     name, path = parse_and_save_csv(input.csv_content, input.table_name)
@@ -16,7 +17,10 @@ def wrapper_save_csv(input: TableOutput):
 
 # Define the processing chain
 markdown_processor_chain = (
-    {"table": RunnablePassthrough(), "src_input": RunnablePassthrough()}
+      RunnableLambda(lambda x: {
+        "table": x["table"].lower(),
+        "src_input": x["src_input"]
+    })
     | parse_csv_promt
     | llm
     | wrapper_save_csv
